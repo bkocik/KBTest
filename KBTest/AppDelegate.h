3a1a5ee3a3fe0069a3ecf5dbb241d2e8711172d5
Bill Kocik <bkocik@gmail.com>
2013-04-12 17:05:45 -0400

    Initial commit.

---
+++ KBTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KBTest
+//
+//  Created by Bill Kocik on 4/12/13.
+//  Copyright (c) 2013 Bill Kocik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KBTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KBTest
+//
+//  Created by Bill Kocik on 4/12/13.
+//  Copyright (c) 2013 Bill Kocik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
